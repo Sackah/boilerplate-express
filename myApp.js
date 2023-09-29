17ae7d357340778dd39b54e489e6f8117ea7255b
Sackah <81268132+Sackah@users.noreply.github.com>
2023-09-29 12:44:38 +0000

    Step 3

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", (req, res)=>{
-    res.send("Hello Express");
+    //res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
